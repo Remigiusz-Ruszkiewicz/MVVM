ab209b43a0543f72ce5347693ed34480ce1f9930
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-10-31 19:38:48 +0100

    Add project files.

---
+++ MVVM/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+
+    sealed class MainViewModel : INotifyPropertyChanged
+    {
+        private User user;
+        public MainViewModel()
+        {
+            user = new User
+            {
+                FirstName = FirstName,
+                LastName = LastName
+            };
+        }
+        public string FullName => firstName + " " + lastName;
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChange("firstName");
+                    OnPropertyChange("FullName");
+                }
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChange("lastName");
+                    OnPropertyChange("FullName");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ MVVM/User.cs
@@ -0,0 +1,8 @@
+﻿namespace MVVM
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
